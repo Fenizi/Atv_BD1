3063b6889004a375eb284c70b93f25b82cda5d43
Pedro H. <96092387+Fenizi@users.noreply.github.com>
2025-03-12 22:26:27 -0300

    Add files via upload

---
+++ ExercBD03.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Exerc03;
+
+USE Exerc03;
+CREATE TABLE Funcionario(
+CodFunc int,
+NomeFunc varchar(50),
+CodDept int,
+Ramal int Null,
+Salario double,
+DataAdmissao datetime,
+DataCadastro datetime,
+Sexo char(1))
